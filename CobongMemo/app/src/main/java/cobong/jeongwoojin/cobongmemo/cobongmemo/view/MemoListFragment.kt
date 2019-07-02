f946dbb45b1c09c034ffa48666a2a5bc14a6f77e
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-02 14:23:12 +0900

    #1- MemoListFragment 수정

--- CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/MemoListFragment.kt
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/MemoListFragment.kt
@@ -1,6 +1,7 @@
 package cobong.jeongwoojin.cobongmemo.cobongmemo.view
 
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -30,8 +31,9 @@ import com.google.android.material.snackbar.Snackbar
 class MemoListFragment : Fragment(), View.OnClickListener, MemoNavigator {
 
     //DB
-    private var binding: FragmentMemoListBinding? = null
+    private lateinit var binding: FragmentMemoListBinding
     private lateinit var viewModel: MemoViewModel
+    private lateinit var memoAdapter: MemoAdapter
 
     override fun onStart() {
         super.onStart()
@@ -44,45 +46,45 @@ class MemoListFragment : Fragment(), View.OnClickListener, MemoNavigator {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_memo_list, container, false)
 
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_memo_list, container, false)
         viewModel = ViewModelProviders.of(this).get(MemoViewModel::class.java)
 
 
-        viewModel!!.navigator = this
+        viewModel.navigator = this
 
         //RecyclerView init
         initRecyclerView()
 
         //스크롤시 fab 없애기
-        binding!!.rcvMemoList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        binding.rcvMemoList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
 
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
 
                 if (newState == SCROLL_STATE_DRAGGING) {
-                    binding!!.fabAdd.hide()
+                    binding.fabAdd.hide()
                 } else if (newState == SCROLL_STATE_IDLE) {
 
                     val anim = AnimationUtils.loadAnimation(context, R.anim.delay)
-                    binding!!.fabAdd.animation = anim
-                    binding!!.fabAdd.show()
+                    binding.fabAdd.animation = anim
+                    binding.fabAdd.show()
                 }
             }
         })
 
+        binding.fabAdd.setOnClickListener(this)
 
-        binding!!.fabAdd.setOnClickListener(this)
-
-
-        return binding!!.root
+        return binding.root
     }
 
     fun initRecyclerView() {
-        //memoAdapter = MemoAdapter(ArrayList(), viewModel)
-        viewModel.initAdapter()
-        binding!!.rcvMemoList.layoutManager = LinearLayoutManager(context)
-        binding!!.rcvMemoList.adapter = viewModel.getAdapter()
+
+        viewModel.getAllMemos()
+
+        memoAdapter = MemoAdapter(viewModel.allMemos, viewModel)
+        binding.rcvMemoList.layoutManager = LinearLayoutManager(context)
+        binding.rcvMemoList.adapter = memoAdapter
     }
 
 
@@ -90,47 +92,55 @@ class MemoListFragment : Fragment(), View.OnClickListener, MemoNavigator {
     fun loadList() {
 
         viewModel.getAllMemos()
-        viewModel.getAdapter()!!.setItem(viewModel.allMemos)
+        memoAdapter.setItem(viewModel.allMemos)
+        //viewModel.getAdapter()!!.setItem(viewModel.allMemos)
 
     }
 
-    //클릭 이벤트
     override fun onClick(v: View) {
         when (v.id) {
             R.id.fab_add -> {
 
-                val memoType = arrayOf<CharSequence>("글", "음성", "손글씨")
-                val builder = AlertDialog.Builder(context)
-                builder.setTitle("메모 타입 선택")
-                builder.setItems(memoType) { dialog, which ->
-                    when (which) {
+                val memoType = arrayOf<CharSequence>(
+                    context?.resources!!.getString(R.string.text),
+                    context?.resources!!.getString(R.string.voice),
+                    context?.resources!!.getString(R.string.handwrite)
+                )
 
-                        //글
-                        0 -> {
-                            val intent = Intent(activity, TextMemoWriteActivity::class.java)
-                            intent.putExtra("type", "addmemo")
-                            startActivity(intent)
-                        }
+                AlertDialog.Builder(context).apply {
+                    setTitle(context?.resources!!.getString(R.string.select_memo_type))
+                    setItems(memoType) { _, which ->
+                        when (which) {
 
-                        //음성
-                        1 -> {
+                            //글
+                            0 -> {
+                                val intent = Intent(activity, TextMemoWriteActivity::class.java)
+                                intent.putExtra("type", "addmemo")
+                                startActivity(intent)
+                            }
 
-                            val dialogFragment = VoiceRecordFragment()
-                            //Fragment 종료시 화면 새로고침
-                            dialogFragment.setOnDismissListener { onStart() }
-                            dialogFragment.show(activity!!.supportFragmentManager, "tag")
-                        }
+                            //음성
+                            1 -> {
+                                val dialogFragment = VoiceRecordFragment()
+                                //Fragment 종료시 화면 새로고침
+                                dialogFragment.setOnDismissListener(
+                                    DialogInterface.OnDismissListener() {
+                                       onStart()
+                                    }
+                                )
 
-                        2 -> {
-                            val intent3 = Intent(activity, HandwritingActivity::class.java)
-                            intent3.putExtra("type", "insert")
-                            startActivity(intent3)
+                                dialogFragment.show(activity!!.supportFragmentManager, "tag")
+                            }
+
+                            2 -> {
+                                val intent3 = Intent(activity, HandwritingActivity::class.java)
+                                intent3.putExtra("type", "insert")
+                                startActivity(intent3)
+                            }
                         }
                     }
-                }
 
-                val alertDialog = builder.create()
-                alertDialog.show()
+                }.create().apply { show() }
             }
         }
 
@@ -139,74 +149,91 @@ class MemoListFragment : Fragment(), View.OnClickListener, MemoNavigator {
 
     //메모 보기로 이동
     override fun sendMemo(item: MemoListItem) {
-
-        val intent: Intent
-
-        if (item.memoType == "text") {
-            intent = Intent(context, TextMemoViewActivity::class.java)
-            intent.putExtra("textItem", item)
-            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(intent)
-        } else if (item.memoType == "voice") {
-
-            val dialogFragment = VoicePlayFragment()
-            val inputdate = Bundle()
-            inputdate.putString("inputdate", item.voiceId)
-            dialogFragment.arguments = inputdate
-            dialogFragment.show(activity!!.supportFragmentManager, "tag")
-
-        } else {
-            intent = Intent(context, HandwriteViewActivity::class.java)
-            intent.putExtra("handwriteItem", item)
-            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-            startActivity(intent)
-        }
-
-
+        selectMemoType(item,
+            {
+                val intent = Intent(context, TextMemoViewActivity::class.java)
+                intent.putExtra("textItem", item)
+                intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+            }, {
+                val dialogFragment = VoicePlayFragment()
+                val inputdate = Bundle()
+                inputdate.putString("inputdate", item.voiceId)
+                dialogFragment.arguments = inputdate
+                dialogFragment.show(activity!!.supportFragmentManager, "tag")
+            }, {
+                val intent = Intent(context, HandwriteViewActivity::class.java)
+                intent.putExtra("handwriteItem", item)
+                intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            }
+        )
     }
 
 
     override fun deleteMemo(memo: MemoListItem) {
 
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle("경고")
-            .setMessage("메모를 지우시겠습니까?")
-            .setPositiveButton("닫기", null)
-            .setNegativeButton("확인") { dialog, which ->
+        makeDialog {
+            //delete memo
+            viewModel.deleteMemo(memo, memoAdapter)
 
-                //delete memo
-                viewModel.deleteMemo(memo)
-            }
-
-        val alertDialog = builder.create()
-        alertDialog.setCanceledOnTouchOutside(false)
-        alertDialog.show()
-
+            memoAdapter.removeItem(memo)
+        }
     }
 
 
     override fun editMemo(memo: MemoListItem) {
 
-        val intent: Intent
+        selectMemoType(memo,
+            {
+                val intent = Intent(activity, TextMemoWriteActivity::class.java)
+                intent.putExtra("textItem", memo)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            },
+            { Snackbar.make(view!!, "수정할 수 없습니다", Snackbar.LENGTH_SHORT).show() },
+            {
+                val intent = Intent(activity, HandwritingActivity::class.java)
+                intent.putExtra("handwriteItem", memo)
+                intent.putExtra("type", "edit")
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            }
+        )
+    }
 
-        if (memo.memoType == "text") {
-            intent = Intent(activity, TextMemoWriteActivity::class.java)
-            intent.putExtra("textItem", memo)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-            startActivity(intent)
-        } else if (memo.memoType == "handwrite") {
-            intent = Intent(activity, HandwritingActivity::class.java)
-            intent.putExtra("handwriteItem", memo)
-            intent.putExtra("type", "edit")
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-            startActivity(intent)
-        } else {
-            Snackbar.make(view!!, "수정할 수 없습니다", Snackbar.LENGTH_SHORT).show()
+    fun selectMemoType(
+        memo: MemoListItem, text: () -> Unit, voice: () -> Unit,
+        handwrite: () -> Unit
+    ) {
+        when (memo.memoType) {
+            "text" -> text()
+
+            "voice" -> voice()
+
+            else -> handwrite()
         }
     }
 
+    fun makeDialog(dialogProcess: () -> Unit) {
+        AlertDialog.Builder(context).apply {
+
+            this.setTitle("경고")
+            this.setMessage("메모를 지우시겠습니까?")
+            this.setPositiveButton("닫기", null)
+            setNegativeButton("확인") { _, _ ->
+
+                dialogProcess()
+
+            }
+
+        }.create().apply {
+            setCanceledOnTouchOutside(false)
+            show()
+        }
+    }
 
 }
