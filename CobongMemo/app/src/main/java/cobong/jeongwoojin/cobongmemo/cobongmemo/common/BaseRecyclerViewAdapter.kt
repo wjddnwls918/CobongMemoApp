aacdc2f0bf86ecab6d07c0ab46ac7a1bc4b6eab4
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-02 20:56:23 +0900

    #1- BaseRecyclerView 수정

--- CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/common/BaseRecyclerViewAdapter.kt
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/common/BaseRecyclerViewAdapter.kt
@@ -1,11 +1,10 @@
 package cobong.jeongwoojin.cobongmemo.cobongmemo.common
 
 import androidx.recyclerview.widget.RecyclerView
-import java.util.*
-abstract class BaseRecyclerVIewAdapter<T, VH : RecyclerView.ViewHolder?>(open var dataSet: MutableList<T>?) :
+abstract class BaseRecyclerVIewAdapter<T, VH : RecyclerView.ViewHolder?>(var dataSet: List<T>) :
     RecyclerView.Adapter<VH>() {
     fun getItem(position: Int): T? {
-        return if (dataSet == null) null else dataSet!![position]
+        return if (dataSet.size==0) null else dataSet[position]
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
@@ -15,7 +14,7 @@ abstract class BaseRecyclerVIewAdapter<T, VH : RecyclerView.ViewHolder?>(open va
     abstract fun onBindView(holder: VH, position: Int)
 
     override fun getItemCount(): Int {
-        return if (dataSet == null) 0 else dataSet!!.size
+        return dataSet.size
     }
 
     /**
@@ -23,61 +22,61 @@ abstract class BaseRecyclerVIewAdapter<T, VH : RecyclerView.ViewHolder?>(open va
      *
      * @param items list.
      */
-    fun addItems(items: List<T>) {
-        if (dataSet == null) {
+   /* fun addItems(items: List<T>) {
+        *//*if (dataSet == null) {
             dataSet = ArrayList()
-        }
+        }*//*
 
         val currentSize = itemCount
-        dataSet!!.addAll(items)
+        dataSet.addAll(items)
 
         notifyItemRangeInserted(currentSize, items.size)
     }
 
-    /**
+    *//**
      * clear and add items.
      * @param items list.
-     */
+     *//*
     fun updateItems(items: List<T>) {
-        if (dataSet == null) {
+       *//* if (dataSet == null) {
             dataSet = ArrayList()
-        }
-        dataSet!!.clear()
+        }*//*
+        dataSet.clear()
         notifyDataSetChanged()
 
-        dataSet!!.addAll(items)
+        dataSet.addAll(items)
         notifyItemRangeChanged(0, items.size)
     }
 
 
-    /**
+    *//**
      * RemoveAll items.
-     */
+     *//*
     fun removeAllItems() {
-        if (dataSet != null) {
-            dataSet!!.clear()
+        if (dataSet.size != 0) {
+            dataSet.clear()
             notifyDataSetChanged()
         }
     }
 
-    /**
+    *//**
      * Remove item
-     */
+     *//*
     fun removeItem(position: Int) {
-        dataSet!!.removeAt(position)
+        dataSet.removeAt(position)
         notifyItemRemoved(position)
     }
 
     fun removeItem(item: T) {
         val position = getItemPosition(item)
         removeItem(position)
-    }
+    }*/
 
 
     /**
      * Set items.
      */
-    fun setItem(items: MutableList<T>) {
+    fun setItem(items: List<T>) {
         this.dataSet = items
         notifyDataSetChanged()
     }
@@ -88,7 +87,7 @@ abstract class BaseRecyclerVIewAdapter<T, VH : RecyclerView.ViewHolder?>(open va
      */
 
     fun getItemPosition(item: T): Int {
-        return dataSet!!.indexOf(item)
+        return dataSet.indexOf(item)
     }
 
 
