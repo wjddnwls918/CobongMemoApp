a0e7deeb8bc40d230de95e3d6597ddc456386480
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-06 19:56:58 +0900

    #10- ScheduleNavigator 추가

---
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/schedule/ScheduleNavigator.kt
@@ -0,0 +1,10 @@
+package cobong.jeongwoojin.cobongmemo.cobongmemo.view.schedule
+
+interface ScheduleNavigator {
+    fun onAddScheduleStartClick() {}
+    fun onScheduleWriteFinishClick() {}
+    fun onSetAlarmClick() {}
+    fun onStartTimeSettingClick() {}
+    fun onEndTimeSettingClick() {}
+    fun onDateClick() {}
+}
