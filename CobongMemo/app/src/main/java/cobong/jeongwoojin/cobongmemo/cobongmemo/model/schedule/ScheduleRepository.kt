55cf416334b52d828b2ce1fe53b107784f975026
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-15 15:51:06 +0900

    #9- ScheduleRepository 변경

--- CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/model/schedule/ScheduleRepository.kt
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/model/schedule/ScheduleRepository.kt
@@ -1,36 +1,62 @@
 package cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import cobong.jeongwoojin.cobongmemo.cobongmemo.model.MemoDatabase
 import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.placeinfo.KakaoApiCreator
 import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.placeinfo.KakaoPlaceApi
 import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.placeinfo.KakaoResponseModel
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 
-class ScheduleRepository {
+class ScheduleRepository(application: Application) {
 
     val service: KakaoPlaceApi = KakaoApiCreator.create(KakaoPlaceApi::class.java)
+
+    private val memoDatabase =
+        MemoDatabase.getDatabase(
+            application
+        )
+    private val scheduleDao: ScheduleDao = memoDatabase.scheduleDao()
+
 
     companion object {
         private var INSTANCE: ScheduleRepository? = null
 
-        fun getInstance(): ScheduleRepository {
+        fun getInstance(application: Application): ScheduleRepository {
             val tempInstance =
                 INSTANCE
             if (tempInstance != null) {
                 return tempInstance
             }
             synchronized(ScheduleRepository::class) {
-                val instance = ScheduleRepository()
+                val instance = ScheduleRepository(application)
                 INSTANCE = instance
                 return instance
             }
         }
     }
 
-    fun getKeywordPlace(key: String, query: String, page:Int): Single<KakaoResponseModel> {
+    /*  From kakao api
+    *
+    *
+     */
+    fun getKeywordPlace(key: String, query: String, page: Int): Single<KakaoResponseModel> {
         return service.getKeywordPlace(key, query, page)
             //.observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io());
     }
+
+
+    /*  From Room
+    *
+    *
+     */
+    val schedule: LiveData<List<ScheduleItem>> = scheduleDao.getAllSchedule()
+
+    suspend fun insertByRoom(schedule: ScheduleItem) {
+        scheduleDao.insert(schedule)
+    }
+
 
 }
