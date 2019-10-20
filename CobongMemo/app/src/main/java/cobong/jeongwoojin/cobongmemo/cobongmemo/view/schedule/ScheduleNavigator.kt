956b2d49b8ba5e415aad17b5f8ed9440ef395921
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-10-20 11:17:29 +0900

    #17- ViewModel, Navigator 분리로 ScheduleFragment, ScheduleNavigator, ScheduleViewModel 수정

--- CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/schedule/ScheduleNavigator.kt
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/schedule/ScheduleNavigator.kt
@@ -1,16 +1,9 @@
 package cobong.jeongwoojin.cobongmemo.cobongmemo.view.schedule
 
 import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.ScheduleItem
-import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.placeinfo.Document
 
 interface ScheduleNavigator {
-    fun onAddScheduleStartClick() {}
-    fun onScheduleWriteFinishClick() {}
-    fun onSetAlarmClick() {}
-    fun onStartTimeSettingClick() {}
-    fun onEndTimeSettingClick() {}
-    fun onDateClick() {}
-    fun onDocumentClick(document: Document) {}
-    fun onScheduleClick(schedule: ScheduleItem) {}
-    fun onScheduleDeleteClick() {}
+    fun onAddScheduleStartClick()
+    fun onScheduleClick(schedule: ScheduleItem)
+
 }
