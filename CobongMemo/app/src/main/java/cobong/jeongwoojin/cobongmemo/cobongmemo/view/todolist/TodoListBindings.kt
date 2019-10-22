150f6261b6559ca97b5b950054a371894d931106
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-10-22 16:47:01 +0900

    #23- TodoListBindings 작성

---
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/todolist/TodoListBindings.kt
@@ -0,0 +1,73 @@
+package cobong.jeongwoojin.cobongmemo.cobongmemo.view.todolist
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import cobong.jeongwoojin.cobongmemo.cobongmemo.R
+import cobong.jeongwoojin.cobongmemo.cobongmemo.common.util.DateUtil
+import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.ScheduleItem
+
+/**
+ * [BindingAdapter]s for the [ScheduleItem]s list.
+ */
+@BindingAdapter("app:items")
+fun setItems(listView: RecyclerView, items: List<ScheduleItem>?) {
+    (listView.adapter as? TodoListAdapter)?.submitList(items ?: listOf())
+}
+
+@BindingAdapter("app:remainTime", "app:date")
+fun setRemainTime(textView: TextView, startTime: String, date: String) {
+
+    if (date.equals(DateUtil.getTodayOrTomorrow("today"))) {
+
+        val currentTime = DateUtil.getCurrentTime()
+
+        val currentTrans = currentTime.split(":")
+        val startTimeTrans = startTime.split(":")
+
+        when {
+            startTimeTrans[0].toInt() < currentTrans[0].toInt() -> {
+                textView.setTextColor(textView.resources.getColor(R.color.cobongRed))
+                textView.setText(R.string.todolist_time_over)
+            }
+
+            startTimeTrans[0].toInt() == currentTrans[0].toInt() -> {
+                when {
+                    startTimeTrans[1].toInt() >= currentTrans[1].toInt() -> {
+                        var hour = startTimeTrans[0].toInt() - currentTrans[0].toInt()
+                        var minute = startTimeTrans[1].toInt() - currentTrans[1].toInt()
+
+                        if (minute < 0) {
+                            hour -= 1
+                            minute += 60
+                        }
+
+                        val string = hour.toString() + "시간 " + minute + "분 남았습니다"
+
+                        textView.setText(string)
+                    }
+                    else -> {
+                        textView.setTextColor(textView.resources.getColor(R.color.cobongRed))
+                        textView.setText(R.string.todolist_time_over)
+                    }
+                }
+            }
+            else -> {
+                var hour = startTimeTrans[0].toInt() - currentTrans[0].toInt()
+                var minute = startTimeTrans[1].toInt() - currentTrans[1].toInt()
+
+                if (minute < 0) {
+                    hour -= 1
+                    minute += 60
+                }
+
+                val string = hour.toString() + "시간 " + minute + "분 남았습니다"
+
+                textView.setText(string)
+            }
+        }
+    } else {
+        textView.setText(startTime)
+    }
+
+}
