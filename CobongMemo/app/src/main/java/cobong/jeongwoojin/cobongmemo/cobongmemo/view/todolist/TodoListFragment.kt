0be55ea2d6a18fb8d488044353c3f1b06972633c
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-10-22 16:46:07 +0900

    #23- TodoListFragment 추가

--- CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/todolist/TodoListFragment.kt
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/todolist/TodoListFragment.kt
@@ -1,22 +1,27 @@
 package cobong.jeongwoojin.cobongmemo.cobongmemo.view.todolist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import cobong.jeongwoojin.cobongmemo.cobongmemo.R
+import cobong.jeongwoojin.cobongmemo.cobongmemo.common.EventObserver
 import cobong.jeongwoojin.cobongmemo.cobongmemo.databinding.FragmentTodoListBinding
+import cobong.jeongwoojin.cobongmemo.cobongmemo.model.schedule.ScheduleItem
+import cobong.jeongwoojin.cobongmemo.cobongmemo.view.schedule.scheduleshow.ScheduleShowActivity
 
 class TodoListFragment : Fragment() {
 
     private lateinit var binding: FragmentTodoListBinding
+    private lateinit var viewModelFactory: ViewModelProvider.AndroidViewModelFactory
+    private lateinit var viewModel: TodoListViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var todayTodoListAdapter: TodoListAdapter
+    private lateinit var tomorrowTodoListAdapter: TodoListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +30,46 @@ class TodoListFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_todo_list, container, false)
 
+        viewModelFactory =
+            ViewModelProvider.AndroidViewModelFactory.getInstance(activity!!.application)
+        viewModel =
+            ViewModelProvider(this, viewModelFactory).get(TodoListViewModel::class.java).apply {
+                binding.viewmodel = this
+            }
 
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        setupListAdapter()
+        setupNavigation()
+    }
+
+    private fun setupListAdapter() {
+        if (binding.viewmodel != null) {
+            todayTodoListAdapter = TodoListAdapter(viewModel)
+            tomorrowTodoListAdapter = TodoListAdapter(viewModel)
+            binding.rcvTodayTodoList.adapter = todayTodoListAdapter
+            binding.rcvTomorrowTodoList.adapter = tomorrowTodoListAdapter
+        }
+    }
+
+    private fun setupNavigation() {
+        viewModel.openTaskEvent.observe(this, EventObserver {
+            openTaskDetails(it)
+        })
+
+    }
+
+    private fun openTaskDetails(schedule: ScheduleItem) {
+        startActivity(
+            Intent(
+                context,
+                ScheduleShowActivity::class.java
+            ).apply { this.putExtra("schedule", schedule) })
     }
 
 }
