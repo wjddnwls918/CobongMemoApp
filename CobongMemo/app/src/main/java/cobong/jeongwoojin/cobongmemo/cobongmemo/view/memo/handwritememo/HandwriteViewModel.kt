22794797a7c8ed3977a430affc0bc84dbdddef73
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-10-25 16:05:42 +0900

    #27- 손글씨 이벤트 방식, Viewmodel 변경

--- CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/memo/handwritememo/HandwriteViewModel.kt
+++ CobongMemo/app/src/main/java/cobong/jeongwoojin/cobongmemo/cobongmemo/view/memo/handwritememo/HandwriteViewModel.kt
@@ -2,35 +2,50 @@ package cobong.jeongwoojin.cobongmemo.cobongmemo.view.memo.handwritememo
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import cobong.jeongwoojin.cobongmemo.cobongmemo.MemoApplication
+import cobong.jeongwoojin.cobongmemo.cobongmemo.common.Event
 import cobong.jeongwoojin.cobongmemo.cobongmemo.model.memo.MemoItem
-import cobong.jeongwoojin.cobongmemo.cobongmemo.model.memo.MemoRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class HandwriteViewModel(application: Application) : AndroidViewModel(application) {
 
     var item: MemoItem? = null
-    var navigator: HandwriteNavigator? = null
+
+    //메모 삭제
+    private val _deleteMemoEvent = MutableLiveData<Event<Unit>>()
+    val deleteMemoEvent: LiveData<Event<Unit>> = _deleteMemoEvent
+
+    //뒤로가기
+    private val _exitClickEvent = MutableLiveData<Event<Unit>>()
+    val exitClickEvent: LiveData<Event<Unit>> = _exitClickEvent
+
+    //작성 완료
+    private val _writeClickEvent = MutableLiveData<Event<Unit>>()
+    val writeClickEvent: LiveData<Event<Unit>> = _writeClickEvent
+
 
     //뒤로가기
     fun onExitClick() {
-        navigator!!.onExitClick()
+        _exitClickEvent.value = Event(Unit)
     }
 
     //삭제
     fun onDeleteClick() {
-        navigator!!.onDeleteClick()
+        _deleteMemoEvent.value = Event(Unit)
     }
 
     //작성 완료
     fun onWriteClick() {
-        navigator!!.onWriteClick()
+        _writeClickEvent.value = Event(Unit)
     }
 
     fun insertHandWriteMemoByRoom(title: String, subTitle: String, handwriteId: String) {
         viewModelScope.launch (Dispatchers.IO){
-            MemoRepository.getInstance(getApplication()).insertByRoom(
+            MemoApplication.memoRepository.insertByRoom(
                 MemoItem(
                     index = null,
                     title = title,
@@ -45,7 +60,7 @@ class HandwriteViewModel(application: Application) : AndroidViewModel(applicatio
     }
 
     fun deleteHandwriteMemoByRoom() = viewModelScope.launch(Dispatchers.IO) {
-        MemoRepository.getInstance(getApplication()).deleteMemoNullableByRoom(item)
+        MemoApplication.memoRepository.deleteMemoNullableByRoom(item)
     }
 
 }
